b52016aeb5f499d948c2d5c63c8ac4103906baa1
Bokang Sibolla <bokang.y.sibolla@gmail.com>
2026-02-04 13:03:32 +0800

    feat(explore): add sections barrel export

---
+++ components/explore/sections/index.ts
@@ -0,0 +1,5 @@
+// components/explore/sections/index.ts
+export { SectionLabel } from './SectionLabel';
+export { CountryPairSection } from './CountryPairSection';
+export { CitySpotlightSection } from './CitySpotlightSection';
+export { ActivityClusterSection } from './ActivityClusterSection';
