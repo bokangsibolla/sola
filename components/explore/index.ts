48220745bfc4780ce9942798d24bbcc60c432009
Bokang Sibolla <bokang.y.sibolla@gmail.com>
2026-02-04 13:08:39 +0800

    chore(explore): remove unused tab components

--- components/explore/index.ts
+++ components/explore/index.ts
@@ -1,9 +1,6 @@
-export { default as ExploreCard, DEFAULT_CARD_WIDTH } from './ExploreCard';
-export { default as GridCard, GRID_CARD_WIDTH, GRID_GAP } from './GridCard';
-export { default as FeaturedCard } from './FeaturedCard';
-export { default as SectionHeader } from './SectionHeader';
-export { default as SearchBar } from './SearchBar';
-export { default as SegmentedControl } from './SegmentedControl';
-export { default as IconTabs } from './IconTabs';
-export { default as WelcomeHeader } from './WelcomeHeader';
-export { default as HorizontalCarousel, DEFAULT_ITEM_WIDTH, DEFAULT_GAP } from './HorizontalCarousel';
+// components/explore/index.ts
+export { ExploreFeed } from './ExploreFeed';
+export { FeedItem } from './FeedItem';
+export { FeedEndCard } from './FeedEndCard';
+export * from './cards';
+export * from './sections';
--- components/explore/tabs/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as CountriesTab } from './CountriesTab';
-export { default as PlacesTab } from './PlacesTab';
-export { default as ActivitiesTab } from './ActivitiesTab';
